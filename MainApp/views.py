a0297c1c19c0285c5dc99a0bb3e66b0216669aef
pavel8silber <paul8julia@gmail>
2016-07-06 13:15:37 -0700

    Updates taken from A_Kozlov.

--- MainApp/views.py
+++ MainApp/views.py
@@ -49,29 +49,6 @@ def home(request):
                             "work_places": _work_places},
                             )
 
-##########################################################################################################
-# <ul>
-#             {%for item in high_education %}
-#             <li>{{item.university}}, {{item.department}} отделение, {{item.faculty}} факультет</li>
-#             {%empty %}
-#                 Отсутствует
-#             {%endfor %}
-#         </ul>
-
-
-# def study(request):
-#     title_page='Обучение'
-#     courses=Course.objects.all()
-
-#     high_education=[{'university':'Казанский Государственный Университет им. В. И. Ульянова-Ленина',
-#                      'department':'дневное',
-#                      'faculty':'биолого-почвенный'}]
-
-
-#     return render(request, 'study.html', {'title_page':title_page,  'high_education': high_education, 'courses':courses})
-
-##########################################################################################################
-
 
 def work(request):
     return render(request, 'work.html')
@@ -84,7 +61,7 @@ def contact(request):
     										'email': email,
     										})
 
-    
+
 def base(request):
     return render(request, 'base.html')
 
